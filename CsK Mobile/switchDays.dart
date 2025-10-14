f45be4140ebc8c2387c373addae82124af25e58d
JulezD3v <jul3zdev@gmail.com>
2025-10-14 11:21:32 +0300

    Csk weekly tasks

---
+++ CsK Mobile/switchDays.dart
@@ -0,0 +1,36 @@
+void main(){
+  int day = 3;
+
+  switch(day){
+     case 1 :
+    print("Sunday");
+    break;
+
+    case 2 :
+    print("Monday");
+    break;
+
+    case 3 :
+    print("Tuesday");
+    break;
+
+    case 4 :
+    print("Wednesday");
+    break;
+
+    case 5 :
+    print("Thursday");
+    break;
+
+     case 6 :
+    print("Friday");
+    break;
+
+     case 7 :
+    print("Saturday");
+    break;
+
+    default:
+    print("Please try again");
+  }
+}
