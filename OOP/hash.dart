84ca1ecc41fd4e6687da915dbc59278d68c06d61
JulezD3v <jul3zdev@gmail.com>
2025-11-09 23:33:44 +0300

    classes have different ashcodes on default

---
+++ OOP/hash.dart
@@ -0,0 +1,36 @@
+
+
+void main(){
+  var person1 = Person(
+    name: "Julie",
+    age: 30,
+    residence: "Ottawa",
+  );
+
+  var person2 = Person(
+    name: "Julie",
+    age: 30,
+    residence: "Ottawa",
+  );
+
+  print(person2 ==person1);
+  print(person2.hashCode);
+  print(person1.hashCode);
+
+}
+
+class Person {
+  final String name;
+  final int age;
+  final String residence;
+
+  Person({required this.name, required this.age, required this.residence});
+
+  @override
+  bool operator ==(Object other) {
+    return other is Person && other.name == name;
+  }
+
+  @override
+  int get hashCode => name.hashCode;
+}
